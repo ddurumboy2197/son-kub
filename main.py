48d8854af56cf91dc1717a6e93012cc5dc51c602
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:20:09 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,21 @@
+def kublar(n):
+    for i in range(1, n + 1):
+        print(i ** 3)
+
+kublar(10)
+```
+
+```python
+def kublar(n):
+    for i in range(1, n + 1):
+        print(i ** 3)
+
+kublar(10)
+```
+
+```python
+def kublar(n):
+    for i in range(1, n + 1):
+        print(i ** 3)
+
+kublar(10)
